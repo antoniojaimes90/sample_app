2bd37b72011c9e550e224cdea3a71fbdfcbeeaa4
antoniojaimes90 <antoniojaimes90@gmail.com>
2013-08-11 22:00:35 -0700

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -51,18 +51,21 @@ describe "User pages" do
     end
   end
   
-  describe "signup page" do 
-  	before { visit signup_path }
-  	it { should have_content('Sign up') }
-  	it { should have_title('Sign up') }
-  end
-  
-  describe "profile page" do 
-	let(:user) { FactoryGirl.create(:user) }
-  	before { visit user_path(user) }
-  	 
-  	it { should have_content(user.name) }
-  	it { should have_title(user.name) }
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
   
   describe "signup" do 
@@ -70,11 +73,11 @@ describe "User pages" do
   	
   	let(:submit) { "Create my account" }
   	
-  	describe "with invalid information" do 
-  		it "should not create a user" do 
+  	describe "with invalid information" do
+      it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
-  		end
-  	end
+      end
+    end
   	
   	 describe "with valid information" do
       before do
@@ -84,9 +87,9 @@ describe "User pages" do
         fill_in "Confirmation", with: "foobar"
       end
   		
-  		it "should create a user" do 
-  			expect { click_button submit }.to change(User, :count).by(1)
-  		end
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
   		describe "after saving the user" do 
   				before { click_button submit }
   				let(:user) { User.find_by(email: 'user@example.com') }
